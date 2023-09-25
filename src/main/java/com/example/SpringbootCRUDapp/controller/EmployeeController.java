0a08b6b6f45fe8fce66ea8129d4688cbebb16cce
Hassan_ALi <hassan.ali@dailypay.com>
2023-09-25 10:05:01 +0100

    rest test9

--- src/main/java/com/example/SpringbootCRUDapp/controller/EmployeeController.java
+++ src/main/java/com/example/SpringbootCRUDapp/controller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class EmployeeController {
 
     @Autowired
